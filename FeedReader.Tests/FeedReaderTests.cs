1bf0ea54244c405cde4a850112f281daf7557a11
Joseph Iussa <jiussa@internode.on.net>
2015-09-10 23:09:05 +0930

    Added unit test project.

---
+++ FeedReader.Tests/FeedReaderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FeedReaderTests
+{
+    [TestFixture]
+    public class FeedReaderTests
+    {
+        [Test]
+        public void test()
+        {
+            Assert.Fail();
+        }
+    }
+}
